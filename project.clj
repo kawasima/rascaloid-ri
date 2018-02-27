872fd577f4f423d6641cab27da95cad1451ed797
kawasima <kawasima1016@gmail.com>
2018-02-27 12:39:49 +0900

    Add a docker profile

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject rascaloid-ri "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [net.unit8.darzana/darzana "1.0.0-SNAPSHOT"]]
-  :main darzana.main)
+  :main darzana.main
+  :profiles {:docker {:local-repo "lib"}})
